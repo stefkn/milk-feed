94b9a66ed4645f8a9d0c6969525301e659feb648
stefkn <stefan.nowak@tails.com>
2024-06-04 00:47:23 +0100

    handle light dark mode setting onMount

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -237,6 +237,17 @@
 				bottleSize = value;
 			}
 		});
+
+		// Toggle light/dark mode
+		if (
+			localStorage.theme === "dark" ||
+			(!("theme" in localStorage) &&
+				window.matchMedia("(prefers-color-scheme: dark)").matches)
+		) {
+			document.documentElement.classList.add("dark");
+		} else {
+			document.documentElement.classList.remove("dark");
+		}
 	});
 
 	onDestroy(() => {
