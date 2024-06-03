e8b606dcf6d65e727521f386de2a9d979ef726dc
stefkn <stefan.nowak@tails.com>
2024-06-04 00:42:00 +0100

    feat: add delete button and update styling and functionality in previousFeed.svelte

--- src/components/previousFeed.svelte
+++ src/components/previousFeed.svelte
@@ -1,5 +1,8 @@
 <script>
     import { format } from "@formkit/tempo";
+    import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
 
     export let feed = {
         start: new Date(),
@@ -7,17 +10,19 @@
         duration: 0,
         bottleSize: 0,
     };
+    
+
 </script>
 
 <li
     class="block max-w-2xl m-auto p-2 my-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
 >
-    <div class="flex justify-between">
+    <div class="flex justify-between items-centre gap-2">
         <span>
-            🍼 {format(feed.start, {
-                date: "full",
-                time: "short",
-            })} - {format(feed.end, { time: "short" })}
+            🍼 {format(feed.start, {date: "short"})}
+        </span>
+        <span>
+            {format(feed.start, { time: "short" })} - {format(feed.end, { time: "short" })}
         </span>
         <span>
             {feed.duration} seconds
@@ -25,5 +30,8 @@
         <span>
             {feed.bottleSize}ml
         </span>
+        <button on:click={() => dispatch('deletefeed', feed)} class="bg-red-500 p-1 rounded-md">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
+        </button>
     </div>
 </li>
