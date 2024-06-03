cf45a648a944d01babefedfbd4a5a673bfb6308c
stefkn <stefan.nowak@tails.com>
2024-06-04 00:47:02 +0100

    feat: Add toggleLightDarkMode and toggleNightVision functions to +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -185,10 +185,32 @@
 		});
 	}
 
-	function deleteFeedHistory() {
-		localforage.removeItem("previousFeeds").then(() => {
-			previousFeeds = [];
-		});
+
+	function toggleLightDarkMode() {
+		if (document.documentElement.classList.contains("nv")) {
+			document.documentElement.classList.remove("nv");
+			document.documentElement.classList.add("dark");
+			localStorage.theme = "dark";
+			return;
+		}
+		if (document.documentElement.classList.contains("dark")) {
+			document.documentElement.classList.remove("dark");
+			localStorage.theme = "light";
+		} else {
+			document.documentElement.classList.add("dark");
+			localStorage.theme = "dark";
+		}
+	}
+
+	function toggleNightVision() {
+		if (document.documentElement.classList.contains("nv")) {
+			document.documentElement.classList.remove("nv");
+			localStorage.theme = "dark";
+		} else {
+			document.documentElement.classList.add("nv");
+			document.documentElement.classList.add("dark");
+			localStorage.theme = "nv";
+		}
 	}
 
 	/**
