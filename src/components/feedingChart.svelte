cffaf0394ff249830353cf9f596303c969d89677
stefan <stefan@stefans-MacBook-Pro.local>
2026-08-13 16:17:58 +0100

    fix: bind chart type selector to handler instead of self-invoking

--- src/components/feedingChart.svelte
+++ src/components/feedingChart.svelte
@@ -120,7 +120,7 @@
             >
             <select
                 bind:value={chartType}
-                on:change={updateFeedChart(previousFeeds)}
+                on:change={() => updateFeedChart(previousFeeds)}
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
                 <option value={CHART_FEEDING_TIME}>feeding time</option>
