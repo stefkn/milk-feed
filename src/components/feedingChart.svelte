551c94ca3f4dea55d3800d2513ff41643da0fdc3
stefan <stefan@stefans-MBP.fritz.box>
2026-08-15 07:42:41 +0100

    fix: chart milk consumed instead of bottle size

--- src/components/feedingChart.svelte
+++ src/components/feedingChart.svelte
@@ -45,7 +45,7 @@
             (feed) => feed.duration,
         );
 
-        const previousFeedSizes = previousFeeds.map((feed) => feed.bottleSize);
+        const previousFeedSizes = previousFeeds.map((feed) => milkConsumed(feed));
 
         const previousFeedSpeeds = previousFeeds.map((feed) => {
             if (feed.duration === 0) {
@@ -125,7 +125,7 @@
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
                 <option value={CHART_FEEDING_TIME}>feeding time</option>
-                <option value={CHART_FEEDING_SIZE}>bottle size</option>
+                <option value={CHART_FEEDING_SIZE}>milk consumed (ml)</option>
                 <option value={CHART_FEEDING_SPEED}>feeding speed</option>
             </select>
         </div>
